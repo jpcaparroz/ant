d3bf64225e13a4d69ea66b8a0a50378221e91c23
João Pedro Dias Caparroz <joao.caparroz@netpartners.com.br>
2024-08-16 20:23:00 -0300

    initiate tables creation

--- src/models/category_model.py
+++ src/models/category_model.py
@@ -6,21 +6,19 @@ from sqlalchemy import String
 from sqlalchemy import UUID
 from sqlalchemy import Boolean
 from sqlalchemy import DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 from sqlalchemy.sql import func
 
 from core.config import settings
 
 
-class UserModel(settings.DBBaseModel):
-    __tablename__ = 'user'
+class CategoryModel(settings.DBBaseModel):
+    __tablename__ = 'category'
 
-    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, unique=True)
+    category_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, unique=True)
     name = Column(String(256), nullable=False)
-    email = Column(String(256), nullable=False, unique=True)
-    password = Column(String(256), nullable=False)
-    is_admin = Column(Boolean, default=False)
+    description = Column(String(256), nullable=False, unique=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('user.id'), nullable=False)
+    share = Column(Boolean, default=False)
     created_on = Column(DateTime(timezone=True), server_default=func.now())
     updated_on = Column(DateTime(timezone=True), onupdate=func.now())
-    
-    category = relationship('category', backref='user')
