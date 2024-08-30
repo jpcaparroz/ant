14fc5dc768f6d12a83ada15da2108385c409c628
João Pedro Dias Caparroz <joao.caparroz@netpartners.com.br>
2024-08-30 20:50:54 -0300

    feat: adding payment table and endpoints

--- src/api/v1/api.py
+++ src/api/v1/api.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from api.v1.endpoints import user
 from api.v1.endpoints import category
+from api.v1.endpoints import payment
 
 
 router = APIRouter()
 router.include_router(user.router, prefix='/user', tags=['User'])
 router.include_router(category.router, prefix='/category', tags=['Category'])
+router.include_router(payment.router, prefix='/payment', tags=['Payment'])
--- src/models/payment_model.py
+++ src/models/payment_model.py
@@ -16,8 +16,8 @@ class PaymentModel(settings.DBBaseModel):
     __tablename__ = 'payment'
 
     payment_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, unique=True)
-    name = Column(String(256), nullable=False)
-    description = Column(String(256), nullable=False, unique=True)
+    name = Column(String(256), nullable=False, unique=True)
+    description = Column(String(256), nullable=True)
     user_id = Column(UUID(as_uuid=True), ForeignKey('user.user_id'), nullable=False)
     share = Column(Boolean, nullable=False, default=True)
     active = Column(Boolean, nullable=False, default=True)
