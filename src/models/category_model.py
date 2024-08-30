7d3fa9bb31139f0f32906f98a11c2600f50a7ce2
João Pedro Dias Caparroz <joao.caparroz@netpartners.com.br>
2024-08-30 20:48:33 -0300

    fix: change uniques columns

--- src/models/category_model.py
+++ src/models/category_model.py
@@ -16,8 +16,8 @@ class CategoryModel(settings.DBBaseModel):
     __tablename__ = 'category'
 
     category_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, unique=True)
-    name = Column(String(256), nullable=False)
-    description = Column(String(256), nullable=False, unique=True)
+    name = Column(String(256), nullable=False, unique=True)
+    description = Column(String(256), nullable=True)
     user_id = Column(UUID(as_uuid=True), ForeignKey('user.user_id'), nullable=False)
     share = Column(Boolean, nullable=False, default=True)
     active = Column(Boolean, nullable=False, default=True)
